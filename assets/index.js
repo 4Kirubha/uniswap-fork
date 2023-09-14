98a1fb46a1a784c481d1dea21b7091a519118cc1
4Kirubha <jaimelannister4221@gmail.com>
2023-09-14 11:11:55 +0530

    setup

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+import ether from './ether.png';
+import uniswap from './uniswap.png';
+import search from './Search.png';
+import arrow from './arrow down.png';
+import arrowup from './arrow up.png';
+import close from './cross.png';
+import etherlogo from './etherlogo.png';
+import homeicon from './homeicon.png';
+import lock from './lock.png';
+
+export default {
+  uniswap,
+  search,
+  ether,
+  arrow,
+  arrowup,
+  close,
+  etherlogo,
+  homeicon,
+  lock,
+};
