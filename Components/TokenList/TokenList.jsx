6f577d434f84b86b1b55b7cba6a0c23c1d67ef74
4Kirubha <jaimelannister4221@gmail.com>
2023-09-16 18:04:07 +0530

    setting up token list

--- Components/TokenList/TokenList.jsx
+++ Components/TokenList/TokenList.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import Image from 'next/image';
 
-const TokenList = () => {
+// INTERNAL IMPORT
+import Styles from "./TokenList.module.css";
+import images from "../../assets";
+
+const TokenList = ({ tokenData, setOpenTokenBox }) => {
+  const data = [1,2,3,4,5,6,7];
   return (
-    <div>TokenList</div>
+    <div className={Styles.TokenList}>
+      <p className={Styles.TokenList_close}
+      onClick={() => setOpenTokenBox(false)}>
+        <Image src={images.close}
+          alt='close'
+          width={50}
+          height={50}/>
+      </p>
+      <div className={Styles.TokenList_title}>
+        <h2>Your Token List</h2>
+      </div>
+      {data.map((el,i) =>(
+          <div className={Styles.TokenList_box}>
+            <div className={Styles.TokenList_box_info}>
+              <p className={Styles.TokenList_box_info_symbol}>HEY</p>
+              <p>
+                <span>34</span> GOLD COIN
+              </p>
+            </div>
+          </div>
+        ))}
+    </div>
   )
 }
 
