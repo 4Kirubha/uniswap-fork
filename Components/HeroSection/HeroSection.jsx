bd951ef0dda30b582c6d939048e80a5288023392
4Kirubha <jaimelannister4221@gmail.com>
2023-09-20 17:24:43 +0530

    changed props

--- Components/HeroSection/HeroSection.jsx
+++ Components/HeroSection/HeroSection.jsx
@@ -72,7 +72,7 @@ const HeroSection = ({accounts, tokenData}) => {
         )}
       </div>
 
-      {openSetting && <Token openSetting={openSetting}/>}
+      {openSetting && <Token setOpenSetting={setOpenSetting}/>}
 
       {openToken && (
         <SearchToken
