6944aff0649e4c3da03a1cd1cee0156c98522531
4Kirubha <jaimelannister4221@gmail.com>
2023-09-20 17:25:05 +0530

    setting up toggle

--- Components/Toggle/Toggle.jsx
+++ Components/Toggle/Toggle.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
-const Toggle = () => {
+//INTERNAL IMPORTS
+import Styles from "./Toggle.module.css";
+const Toggle = ({label}) => {
   return (
-    <div>Toggle</div>
+    <div className={Styles.Toggle}>
+      <div className={Styles.Toggle_switch_box}>
+        <input type='checkbox'
+          className={Styles.Toggle_checkbox}
+          name={label}
+          id={label}>
+        </input>
+        <label className={Styles.Toggle_label} htmlFor={label}>
+          <span className={Styles.Toggle_inner}></span>
+          <span className={Styles.Toggle_switch}></span>
+        </label>
+      </div>
+    </div>
   )
 }
 
